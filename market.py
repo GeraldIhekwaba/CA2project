1e8e36e098b53ead33d95b5b21d45391d0e10550
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-03 20:11:00 +0100

    dynamic routing

--- market.py
+++ market.py
@@ -9,3 +9,7 @@ def gerald():
 @app.route("/about")
 def about_page():
     return "<p>Welcome to the about page</p>"
+
+@app.route("/about/<username>")
+def about_page(username):
+    return f"<p>Welcome to the about page of {username}</p>"
