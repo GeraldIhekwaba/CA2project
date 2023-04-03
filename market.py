b7a015c1520a138b0803cbeea961dd84c259b926
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-03 22:06:36 +0100

    nav bar options

--- market.py
+++ market.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def gerald():
+#     return "<h1>Gerald is learning fast!</h1>"
+
+# @app.route("/about")
+# def about_page():
+#     return "<p>Welcome to the about page</p>"
+
+# @app.route("/about/<username>")
+# def about_page_user (username):
+#     return f"<p>Welcome to the about page of {username}</p>"
+
 @app.route("/")
-def gerald():
-    return "<h1>Gerald is learning fast!</h1>"
-
-@app.route("/about")
-def about_page():
-    return "<p>Welcome to the about page</p>"
-
-@app.route("/about/<username>")
-def about_page_user (username):
-    return f"<p>Welcome to the about page of {username}</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home') 
