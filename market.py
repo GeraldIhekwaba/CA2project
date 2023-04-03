a1e8929aea5ee6f06d9edff0a5d4ef09c456aaa5
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-03 20:01:22 +0100

    creating an about page

--- market.py
+++ market.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def gerald():
     return "<h1>Gerald is learning fast!</h1>"
+
+@app.route("/about")
+def about_page():
+    return "<p>Welcome to the about page</p>"
