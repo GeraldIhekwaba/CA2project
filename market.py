e28dda40c7c03ada9be785f299e07cb99193a70a
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-10 00:03:17 +0100

    found the error, I mispelt database in the URI

--- market.py
+++ market.py
@@ -2,9 +2,9 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite://market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
-db.init_app(app)
+
 
 
 class Item(db.Model):
