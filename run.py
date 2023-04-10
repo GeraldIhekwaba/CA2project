cb40b380083df896c7ef19f001e3caaae2a7f6ff
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-10 14:07:57 +0100

    ..

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
+from market import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
+# Checks if the run.py file has been executed directly and not imported
+if __name__ == '__main__':
+    app.run(debug=True)
