07d48f871c0537640cfeb2a3cbb42c517f3e3629
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-11 11:09:20 +0100

    Creating a register template

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '6946e7d7aef3c93d3a6907c2'
 db = SQLAlchemy(app)
 
 
