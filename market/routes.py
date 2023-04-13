b80af6d18eb662155ad04821ff98246e7a068069
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-13 17:15:15 +0100

    creating the login page

--- market/routes.py
+++ market/routes.py
@@ -20,7 +20,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
-                              password=form.password1.data)
+                              password_hash=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -28,3 +28,7 @@ def register_page():
         for error_msg in form.errors.values():
             flash(f'There was an error with creating a user:{error_msg}', category='danger')
     return render_template('register.html',form=form)
+
+@app.route('/login',methods=['GET','POST'])
+def login_page():
+    return render_template('login.html')
