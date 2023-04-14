7fd55e0fc549918315e3a2c8c0a35a226954cb9e
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-14 15:51:06 +0100

    creating functionality for logout

--- market/models.py
+++ market/models.py
@@ -16,8 +16,14 @@ class User(db.Model, UserMixin):
      password_hash = db.Column(db.String(length=60), nullable=False)
      budget = db.Column(db.Float(), nullable=False, default=1000 )
      items = db.relationship('Item', backref='owned_user')
-
-
+     
+     @property
+     def prettier_budget(self):
+         if len(str(self.budget))>= 4:
+             return f'€{str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+         else:
+             return f"€{self.budget}"
+     
      @property
      def password(self):
          return self.password
