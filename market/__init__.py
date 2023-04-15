377d93afb85c3c67a918878f4145a255874635c4
GeraldIhekwaba <tihekwaba@gmail.com>
2023-04-15 15:02:01 +0100

    adding a login required to the market route

--- market/__init__.py
+++ market/__init__.py
@@ -9,6 +9,8 @@ app.config['SECRET_KEY'] = '6946e7d7aef3c93d3a6907c2'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = 'info'
 
 
 from market import routes
